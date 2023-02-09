a2a14c74754d05fe133a4c4f90ac0c6e0e473e83
Boyan Atanasov <91849214+boqan@users.noreply.github.com>
2023-02-09 16:17:28 +0200

    Delete project directory

--- project/backtotopbutton.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.querySelector(".back-to-top").style.display = "block";
-    } else {
-      document.querySelector(".back-to-top").style.display = "none";
-    }
-  };
